51a130f0e77e869605aa73a48b194bebfc23c399
raymondreynoso <r-reynoso@live.com>
2021-06-13 16:59:25 -0400

    Some updates.

--- lib/Screens/Exercise/days.dart
+++ lib/Screens/Exercise/days.dart
@@ -21,65 +21,71 @@ class _DaysState extends State<Days> {
         stream: DatabaseService(uid: user.uid).exerciseData,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Container(
+            return MaterialApp(builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 0.8),
                 child: Scaffold(
-              backgroundColor: Colors.white,
-              appBar: AppBar(
-                iconTheme: IconThemeData(
-                  color: Color.fromARGB(255, 47, 150, 153),
-                ),
-                backgroundColor: Colors.black,
-                elevation: 0.0,
-              ),
-              body: Center(
-                  child: Align(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      // Monday -------------------------------------------------------
-                      SizedBox(height: 20),
-                      SizedBox(
-                        width: 380,
-                        child: Container(
-                          height: 140,
-                          child: Card(
-                            elevation: 2,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                SizedBox(height: 0),
-                                const ListTile(
-                                  leading: Icon(Icons.calendar_today),
-                                  title: Text('Monday'),
-                                  subtitle: Text('WU'),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
+                  backgroundColor: Colors.white,
+                  appBar: AppBar(
+                    iconTheme: IconThemeData(
+                      color: Color.fromARGB(255, 47, 150, 153),
+                    ),
+                    backgroundColor: Colors.black,
+                    elevation: 0.0,
+                  ),
+                  body: Center(
+                      child: Align(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          // Monday -------------------------------------------------------
+                          SizedBox(height: 20),
+                          SizedBox(
+                            width: 380,
+                            child: Container(
+                              height: 140,
+                              child: Card(
+                                elevation: 2,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
                                   children: <Widget>[
-                                    FlatButton(
-                                      child: const Icon(Icons.arrow_forward),
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Monday()),
-                                        );
-                                      },
+                                    SizedBox(height: 0),
+                                    const ListTile(
+                                      leading: Icon(Icons.calendar_today),
+                                      title: Text('Monday'),
+                                      subtitle: Text('WU'),
                                     ),
-                                    const SizedBox(width: 8),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        FlatButton(
+                                          child:
+                                              const Icon(Icons.arrow_forward),
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Monday()),
+                                            );
+                                          },
+                                        ),
+                                        const SizedBox(width: 8),
+                                      ],
+                                    ),
+                                    SizedBox(height: 5),
                                   ],
                                 ),
-                                SizedBox(height: 5),
-                              ],
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  )),
                 ),
-              )),
-            ));
+              );
+            });
           } else {
             return Loading();
           }
--- lib/Screens/Exercise/monday.dart
+++ lib/Screens/Exercise/monday.dart
@@ -21,184 +21,188 @@ class _MondayState extends State<Monday> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             ExerciseData exerciseData = snapshot.data;
-            return Container(
+            return MaterialApp(builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 0.8),
                 child: Scaffold(
-              backgroundColor: Colors.white,
-              appBar: AppBar(
-                iconTheme: IconThemeData(
-                  color: Color.fromARGB(255, 47, 150, 153),
-                ),
-                backgroundColor: Colors.black,
-                elevation: 0.0,
-              ),
-              body: Center(
-                  child: Align(
-                child: Column(
-                  children: <Widget>[
-                    // Monday -------------------------------------------------------
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 380,
-                      child: Container(
-                        height: 500,
-                        child: Card(
-                          elevation: 2,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              SizedBox(height: 0),
-                              const ListTile(
-                                leading: Icon(Icons.calendar_today),
-                                title: Text('Monday'),
-                                subtitle: Text('WU'),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
+                  backgroundColor: Colors.white,
+                  appBar: AppBar(
+                    iconTheme: IconThemeData(
+                      color: Color.fromARGB(255, 47, 150, 153),
+                    ),
+                    backgroundColor: Colors.black,
+                    elevation: 0.0,
+                  ),
+                  body: Center(
+                      child: Align(
+                    child: Column(
+                      children: <Widget>[
+                        // Monday -------------------------------------------------------
+                        SizedBox(height: 20),
+                        SizedBox(
+                          width: 380,
+                          child: Container(
+                            height: 500,
+                            child: Card(
+                              elevation: 2,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise1),
+                                  SizedBox(height: 0),
+                                  const ListTile(
+                                    leading: Icon(Icons.calendar_today),
+                                    title: Text('Monday'),
+                                    subtitle: Text('WU'),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise1),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(
+                                          '--------------------------------------------------------'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise2),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise3),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise4),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text('Rest 1 min'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(
+                                          '--------------------------------------------------------'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise5),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise6),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise7),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text('Rest 1 min'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(
+                                          '--------------------------------------------------------'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise8),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise9),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(exerciseData.mondayExercise10),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text('Stretch'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      const SizedBox(width: 72),
+                                      Text(
+                                          '--------------------------------------------------------'),
+                                    ],
+                                  ),
                                 ],
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(
-                                      '--------------------------------------------------------'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise2),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise3),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise4),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text('Rest 1 min'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(
-                                      '--------------------------------------------------------'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise5),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise6),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise7),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text('Rest 1 min'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(
-                                      '--------------------------------------------------------'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise8),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise9),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(exerciseData.mondayExercise10),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text('Stretch'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  const SizedBox(width: 72),
-                                  Text(
-                                      '--------------------------------------------------------'),
-                                ],
-                              ),
-                            ],
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
+                  )),
                 ),
-              )),
-            ));
+              );
+            });
           } else {
             return Loading();
           }
